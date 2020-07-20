f78cda84c7ce46657c8a1e18a9a6b57a4650b98a
Golden Eagle <joowoon.kang@biola.edu>
2020-07-19 23:53:36 -0700

    delete button is working

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -13,6 +13,19 @@ const ListTodos = () => {
   useEffect(() => {
     getTodos();
   }, []);
+
+  //delete todo function
+  const deleteTodo = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      //   console.log({ res });
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
   return (
     <Fragment>
       <table className="table mt-5">
@@ -31,10 +44,24 @@ const ListTodos = () => {
           </tr> */}
 
           {todos.map((todo) => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button type="button" className="btn btn-info">
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => {
+                    deleteTodo(todo.todo_id);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
